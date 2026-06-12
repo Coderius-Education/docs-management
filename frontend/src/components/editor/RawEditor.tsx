9216356dd6c724979132015b1cbce888f2205517
koen_1711 <koenvurk1711@gmail.com>
2026-06-12 21:19:11 +0200

    chore: add dark mode

--- frontend/src/components/editor/RawEditor.tsx
+++ frontend/src/components/editor/RawEditor.tsx
@@ -1,4 +1,5 @@
 import { markdown } from '@codemirror/lang-markdown';
+import { useComputedColorScheme } from '@mantine/core';
 import CodeMirror, { type ReactCodeMirrorRef } from '@uiw/react-codemirror';
 import { forwardRef } from 'react';
 
@@ -6,12 +7,14 @@ export const RawEditor = forwardRef<
   ReactCodeMirrorRef,
   { value: string; onChange: (value: string) => void }
 >(function RawEditor({ value, onChange }, ref) {
+  const colorScheme = useComputedColorScheme('light');
   return (
     <CodeMirror
       ref={ref}
       value={value}
       onChange={onChange}
       extensions={[markdown()]}
+      theme={colorScheme}
       height="100%"
       style={{ height: '100%', fontSize: 13 }}
     />
--- frontend/src/components/editor/WysiwygEditor.tsx
+++ frontend/src/components/editor/WysiwygEditor.tsx
@@ -1,5 +1,6 @@
 import '@milkdown/crepe/theme/common/style.css';
 import '@milkdown/crepe/theme/frame.css';
+import './WysiwygEditor.css';
 
 import { Crepe } from '@milkdown/crepe';
 import { useEffect, useRef } from 'react';
